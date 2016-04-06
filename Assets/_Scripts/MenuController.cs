adbe8980b2bdb26bf8c68cda88b3d82bee364b7a
ralhpreet <preetralh07@gmail.com>
2016-04-06 00:19:35 -0400

    Assets added in Luvpreet folder and main menu added
    Luvpreet folder is in sprites folder

---
+++ Assets/_Scripts/MenuController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Event Handler for StartButtonClick
+
+	public void StartButtonClick() {
+		SceneManager.LoadScene ("Level1");
+	}
+
+	public void InstructionsButtonClick() {
+		SceneManager.LoadScene ("Instructions");
+	}
+
+	public void LevelsButtonClick() {
+		SceneManager.LoadScene ("Levels");
+	}
+
+	public void QuitButtonClick() {
+		Application.Quit();
+	}
+		
+}
